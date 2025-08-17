b39d089aa52002cbfdf828afaef86eed885c4f09
fieldsa83 <fieldsa83@gmail.com>
2025-08-17 11:04:06 -0400

    Update create_plot.R

--- create_plot.R
+++ create_plot.R
@@ -51,7 +51,7 @@ plot_ly(height=500,width=750) %>%
       title = list(text = "Month", standoff = 15),
       rangeslider = list(visible = TRUE, thickness = 0.08),
       # Use the larger dataset for the max range to ensure it covers both
-      range = c("2019-01-01", as.character(max(df_full$Date)+20))
+      range = c("2019-01-01", as.character(max(df_full_avg_week$Date)+7))
     ),
     yaxis = list(
       title = list(text = "lbs", standoff = 10), 
